ed46d01557b248b398b5c5c7de6893779f9ee221
Aurelien Jabot <aurelien.jabot@gmail.com>
2019-06-03 09:22:23 -0400

    Fix block display issue

--- components/Block.js
+++ components/Block.js
@@ -3,19 +3,22 @@ import { StyleSheet, View } from 'react-native'
 import Tile from './Tile'
 
 export default class Block extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.numbers = [...props.numbers]
-    }
-
     renderTiles(rowIndex) {
         const xIndex = this.props.xIndex * 3
         const yIndex = this.props.yIndex * 3 + rowIndex
-
+        let blockIndex = rowIndex * 3
         let tiles = [];
+
         for (i = xIndex; i < (xIndex + 3); i++) {
-            tiles.push(<Tile xIndex={i} yIndex={yIndex} content={i + rowIndex} key={i} clickEvent={this.props.clickEvent}/>)
+            tiles.push(<Tile
+                key={i}
+                xIndex={i}
+                yIndex={yIndex}
+                content={this.props.numbers[blockIndex]}
+                clickEvent={this.props.clickEvent}
+                isHighlighted={this.props.highlightedRow == i || this.props.highlightedColumn == yIndex}
+            />)
+            blockIndex++
         }
         return tiles;
     }
--- components/Grid.js
+++ components/Grid.js
@@ -33,14 +33,14 @@ export default class Grid extends React.Component {
 
         let blocks = [
             [2, 5, 0, 0, 7, 9, 0, 0, 0],
-            [0, 0, 1, 0, 9, 0, 0, 0, 0],
+            [0, 0, 1, 0, 3, 0, 0, 0, 0],
             [0, 0, 0, 5, 0, 0, 7, 0, 2],
             [0, 0, 0, 3, 0, 0, 0, 6, 0],
             [0, 0, 0, 8, 5, 2, 0, 0, 0],
             [0, 7, 0, 0, 0, 1, 0, 0, 0],
             [9, 0, 6, 0, 0, 1, 0, 0, 0],
             [0, 0, 0, 0, 2, 0, 9, 0, 0],
-            [0, 0, 0, 9, 6, 8, 0, 3, 5],
+            [0, 0, 0, 6, 8, 0, 0, 3, 5],
         ]
 
         this.state = {
@@ -61,22 +61,21 @@ export default class Grid extends React.Component {
 
     render() {
         this.onTileClick = this.onTileClick.bind(this)
-        
         return (
             <View style={styles.container}>
                 <View style={styles.row}>
                     <Block xIndex={0} yIndex={0} numbers={this.state.blocks[0]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
-                    <Block xIndex={0} yIndex={1} numbers={this.state.blocks[1]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
-                    <Block xIndex={0} yIndex={2} numbers={this.state.blocks[2]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
+                    <Block xIndex={1} yIndex={0} numbers={this.state.blocks[1]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
+                    <Block xIndex={2} yIndex={0} numbers={this.state.blocks[2]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
                 </View>
                 <View style={styles.row}>
-                    <Block xIndex={1} yIndex={0} numbers={this.state.blocks[3]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
+                    <Block xIndex={0} yIndex={1} numbers={this.state.blocks[3]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
                     <Block xIndex={1} yIndex={1} numbers={this.state.blocks[4]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
-                    <Block xIndex={1} yIndex={2} numbers={this.state.blocks[5]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
+                    <Block xIndex={2} yIndex={1} numbers={this.state.blocks[5]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
                 </View>
                 <View style={styles.row}>
-                    <Block xIndex={2} yIndex={0} numbers={this.state.blocks[6]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
-                    <Block xIndex={2} yIndex={1} numbers={this.state.blocks[7]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
+                    <Block xIndex={0} yIndex={2} numbers={this.state.blocks[6]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
+                    <Block xIndex={1} yIndex={2} numbers={this.state.blocks[7]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
                     <Block xIndex={2} yIndex={2} numbers={this.state.blocks[8]} highlightedColumn={this.state.highlightedColumn} highlightedRow={this.state.highlightedRow} clickEvent={this.onTileClick}/>
                 </View>
             </View>
