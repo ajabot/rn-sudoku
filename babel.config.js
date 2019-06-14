30182183c2cdeb11c495c29b7b3f467163ffbfc6
Aurelien Jabot <aurelien.jabot@gmail.com>
2019-06-13 20:45:39 -0400

    Android build

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['react-native'],
+    plugins: [
+      "@babel/plugin-transform-runtime"
+    ]
   };
 };
