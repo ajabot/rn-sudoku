aa0bfa0d3c207a6ae35f82880850373b6275e7ab
Aurelien Jabot <aurelien.jabot@gmail.com>
2019-07-01 19:14:02 +0200

    Tile component refactor

--- components/grid/Block.js
+++ components/grid/Block.js
@@ -4,9 +4,7 @@ import { connect } from 'react-redux'
 import { getBlock } from "../../selectors/selectors"
 import { fillTile } from "../../actions/actions";
 
-import Tile from './tiles/Tile'
-import HighlightedTile from './tiles/HighlightedTile'
-import SelectedTile from './tiles/SelectedTile'
+import Tile from './Tile'
 
 class Block extends React.Component {
     renderTiles(rowIndex) {
@@ -14,30 +12,25 @@ class Block extends React.Component {
         const yIndex = this.props.yIndex * 3
         let blockIndex = rowIndex * 3
         let tiles = [];
+        let style = 'default';
 
         for (i = yIndex; i < (yIndex + 3); i++) {
             if (this.props.highlightedRow == xIndex && this.props.highlightedColumn == i) {
-                tiles.push(<SelectedTile
-                    key={i}
-                    xIndex={xIndex}
-                    yIndex={i}
-                    content={this.props.numbers[blockIndex]}
-                />)
-            } else if (this.props.highlightedRow == xIndex || this.props.highlightedColumn == i){
-                tiles.push(<HighlightedTile
-                    key={i}
-                    xIndex={xIndex}
-                    yIndex={i}
-                    content={this.props.numbers[blockIndex]}
-                />)
+                style = 'selected'
+            } else if (this.props.highlightedRow == xIndex || this.props.highlightedColumn == i) {
+                style = 'highlighted'
             } else {
-                tiles.push(<Tile
-                    key={i}
-                    xIndex={xIndex}
-                    yIndex={i}
-                    content={this.props.numbers[blockIndex]}
-                />)
+                style = 'default'
             }
+         
+            tiles.push(<Tile
+                key={i}
+                xIndex={xIndex}
+                yIndex={i}
+                content={this.props.numbers[blockIndex]}
+                style={style}
+            />)
+    
             blockIndex++
         }
         return tiles;
@@ -73,7 +66,9 @@ const styles = StyleSheet.create({
 });
 
 const mapStateToProps = (state, ownProps) => {
-    return { numbers: [...getBlock(state, ownProps.blockIndex)] };
+    return {
+        numbers: [...getBlock(state, ownProps.blockIndex)]
+    };
 };
 
 export default connect(mapStateToProps, {fillTile})(Block);
---
+++ components/grid/Tile.js
@@ -0,0 +1,61 @@
+import React, { Component, useMemo } from 'react'
+import { connect } from 'react-redux'
+import { StyleSheet, Text, View, TouchableWithoutFeedback } from 'react-native'
+import { selectTile } from "../../actions/actions";
+
+let tileBackgroundColor
+
+const Tile = (props) => {
+    let content = props.content > 0 ? props.content : '';
+    
+    const tileStyle = useMemo(
+        () => {
+            switch (props.style) {
+                case 'selected':
+                    return styles.selected
+                    break
+                case 'highlighted':
+                    return styles.highlighted
+                    break
+                default:
+                    return styles.tile
+            }
+        },
+        [props.style]
+    );
+
+    return (
+        <TouchableWithoutFeedback onPress={() => { props.selectTile(props.xIndex, props.yIndex) }}>
+            <View style={tileStyle}>
+                <Text>
+                    {content}
+                </Text>
+            </View>
+        </TouchableWithoutFeedback>
+    )
+}
+
+const commonStyle = {
+    borderWidth: 1,
+    flex: 1,
+    aspectRatio: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+}
+
+const styles = StyleSheet.create({
+    tile: {
+        ...commonStyle,
+        backgroundColor: '#fff'
+    },
+    selected: {
+        ...commonStyle,
+        backgroundColor: 'silver'
+    },
+    highlighted: {
+        ...commonStyle,
+        backgroundColor: 'skyblue'
+    },
+});
+
+export default connect(null, { selectTile })(Tile)
