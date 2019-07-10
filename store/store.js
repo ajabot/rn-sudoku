8024a3e4083338a058f9dff09c2d5dbcfd84eb89
Aurelien Jabot <aurelien.jabot@gmail.com>
2019-07-10 11:45:55 -0400

    Hook redux to React Native Debugger

--- store/store.js
+++ store/store.js
@@ -1,4 +1,10 @@
-import { createStore } from "redux"
+import { createStore, compose } from "redux"
 import gridReducer from "../reducers/gridReducer"
 
-export default createStore(gridReducer)
+let composeEnhancers = compose
+
+if (__DEV__) {
+    composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose    
+}
+
+export default createStore(gridReducer, composeEnhancers())
